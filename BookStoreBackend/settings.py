aa9b127c43f655d841153d08ee0ea81e0165d72f
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-07-23 13:51:16 +0300

    Angular Bookstore Django Backend

--- BookStoreBackend/settings.py
+++ BookStoreBackend/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     # My apps
     'rest_framework',
     'bookStoreAPI',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,3 +126,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:4200'
+]
+CORS_ALLOWED_ORIGIN=[
+    'http://localhost:4200',
+]
