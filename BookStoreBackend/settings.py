50cd5b25257e592e898193eb62cfcae443281322
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-08-20 22:49:10 +0300

    Book REST API Hosting

--- BookStoreBackend/settings.py
+++ BookStoreBackend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 from dj_database_url import parse
@@ -85,7 +85,8 @@ WSGI_APPLICATION = 'BookStoreBackend.wsgi.application'
 #     }
 # }
 
-default_dburl = 'sqlite:///' + BASE_DIR / 'db.sqlite3'
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
 DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=parse)
 }
@@ -126,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
